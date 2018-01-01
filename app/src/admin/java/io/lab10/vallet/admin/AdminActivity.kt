bb015be621f3abcda09565117e140d025e66cb42
Robert Mitwicki <robert@lab10.io>
2018-01-01 23:58:31 +0100

    Add BT scanning functionality to admin and client

--- app/src/admin/java/io/lab10/vallet/admin/AdminActivity.kt
+++ app/src/admin/java/io/lab10/vallet/admin/AdminActivity.kt
@@ -1,6 +1,11 @@
 package io.lab10.vallet.admin
 
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.Parcelable
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
@@ -8,11 +13,45 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import io.lab10.vallet.R
 import kotlinx.android.synthetic.admin.activity_admin.*
 import kotlinx.android.synthetic.admin.app_bar_admin.*
+import android.content.IntentFilter
+import android.bluetooth.BluetoothAdapter
+import android.util.Log
+
 
 class AdminActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    val TAG = "AdminActivity"
+    val broadCastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(contxt: Context?, intent: Intent?) {
+
+            when (intent?.action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    val device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE) as BluetoothDevice
+                    Toast.makeText(applicationContext, "Found device " + device.name, Toast.LENGTH_LONG)
+                    Log.d(TAG, "Found device")
+                    device.fetchUuidsWithSdp()
+                }
+                BluetoothDevice.ACTION_UUID -> {
+                    val uuids = intent.getParcelableArrayExtra(BluetoothDevice.EXTRA_UUID) as Array<Parcelable>
+                    for (uuid in uuids) {
+                        Log.d(TAG, "Found UUID " + uuid)
+                        val address = uuid.toString()
+                        Toast.makeText(applicationContext, "Found address: " + address, Toast.LENGTH_LONG).show()
+                    }
+                }
+                BluetoothAdapter.ACTION_DISCOVERY_STARTED -> {
+                    Log.d(TAG, "Started scanning")
+                }
+                BluetoothAdapter.ACTION_DISCOVERY_FINISHED -> Log.d(TAG, "Scanning is done")
+            }
+
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +69,17 @@ class AdminActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+        // Register for broadcasts when a device is discovered.
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        filter.addAction(BluetoothDevice.ACTION_UUID)
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
+        registerReceiver(broadCastReceiver, filter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(broadCastReceiver)
     }
 
     override fun onBackPressed() {
@@ -51,9 +101,10 @@ class AdminActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         when (item.itemId) {
-            R.id.action_settings -> return true
+            R.id.action_scan -> startScanningForAddresses()
             else -> return super.onOptionsItemSelected(item)
         }
+        return true
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -81,5 +132,26 @@ class AdminActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    fun startScanningForAddresses() {
+
+        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(applicationContext, "This device does not support BT can't use scanning", Toast.LENGTH_LONG)
+            return
+        }
+        if (!mBluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, 1)
+            // TODO handle turn on off in activity
+            // see https://developer.android.com/guide/topics/connectivity/bluetooth.html
+        }
+        val btStarted = mBluetoothAdapter.startDiscovery()
+        if(btStarted) {
+            Toast.makeText(this, "Start scanning for addresses nearby", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this,"Something went wrong", Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/client/java/io/lab10/vallet/ClientActivity.kt
+++ app/src/client/java/io/lab10/vallet/ClientActivity.kt
@@ -1,11 +1,17 @@
 package io.lab10.vallet
 
+import android.bluetooth.BluetoothAdapter
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import io.lab10.vallet.R
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import io.lab10.vallet.connectivity.BTUtils
 
 import kotlinx.android.synthetic.client.activity_client.*
+
+
 
 class ClientActivity : AppCompatActivity() {
 
@@ -18,6 +24,30 @@ class ClientActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.menuGetVoucher -> startBroadcastingAddress()
+        }
+        return true
+    }
+
+    fun startBroadcastingAddress() {
+        // TODO make sure that BT is on if not turn it on
+        Toast.makeText(this,"Broadcasting address", Toast.LENGTH_LONG).show()
+        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        // TODO store eth address in preferences
+        val address = "Lab10Vallet"
+        val uuid = BTUtils.encodeServiceName(address)
+        val btSocket = mBluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(getString(R.string.app_name), uuid)
+
     }
 
 }
