c322e3a7c59af0f7024fa40bacd809454b5f4b17
Robert Mitwicki <robert@lab10.io>
2018-01-03 16:37:28 +0100

    Add Voucher Activity
    `VoucherActivity` is used to guide admin user to create new voucher.
    In addition there is `MainActivity` which triggers `VoucherActivity`
    first time when user start the app to make sure that at least one
    voucher is created.
    
    Later on there will be possibility to add new vouchers from the
    settings.

---
+++ app/src/admin/java/io/lab10/vallet/admin/WelcomeActivity.kt
@@ -0,0 +1,29 @@
+package io.lab10.vallet.admin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.content.Intent
+import io.lab10.vallet.R
+
+
+class WelcomeActivity : AppCompatActivity() {
+
+    val FIRST_RUN = "FIRST_RUN"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(baseContext)
+        val firstRun = sharedPreferences.getBoolean("FIRST_RUN", false)
+        if (firstRun) {
+            // TODO Start VoucherActivity
+            val intent = Intent(this, VoucherActivity::class.java)
+            startActivity(intent)
+        } else {
+            val intent = Intent(this, AdminActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
