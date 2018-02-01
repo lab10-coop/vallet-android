6baa1b891aee9682c3d88c9397add35092941caf
Robert Mitwicki <robert@lab10.io>
2018-02-01 22:58:14 +0100

    Add simple unit tests for IPFS
    Make sure that we will run it against well defined version of IPFS.
    Same version should be used for production and staging to avoid
    potential issues.

---
+++ app/src/androidTest/java/io/lab10/vallet/IPFSUnitTest.kt
@@ -0,0 +1,35 @@
+package io.lab10.vallet
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import io.ipfs.kotlin.IPFS
+import io.ipfs.kotlin.model.VersionInfo
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by mtfk on 01.02.18.
+ */
+@RunWith(AndroidJUnit4::class)
+class IPFSUnitTest {
+
+    private var ipfsServer : IPFS? = null
+
+    @Before
+    fun setIpfsServer() {
+        val appContext = InstrumentationRegistry.getTargetContext()
+        ipfsServer = IPFSManager.INSTANCE.getIPFSConnection(appContext)
+    }
+
+    @Test
+    fun ipfs_testIPFSVersion() {
+        Assert.assertNotNull(ipfsServer)
+        val version = ipfsServer!!.info.version() as VersionInfo
+
+        Assert.assertEquals("0.4.13", version.Version)
+        Assert.assertEquals("6", version.Repo)
+
+    }
+}
