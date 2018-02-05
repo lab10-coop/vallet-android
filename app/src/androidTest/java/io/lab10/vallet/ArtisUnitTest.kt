edb8fc1a103c11a7092dc1afe1c2ed2e0c2d3bbf
Robert Mitwicki <robert@lab10.io>
2018-02-05 23:20:46 +0100

    Interface for creating new wallet

--- app/src/androidTest/java/io/lab10/vallet/ArtisUnitTest.kt
+++ app/src/androidTest/java/io/lab10/vallet/ArtisUnitTest.kt
@@ -1,5 +1,6 @@
 package io.lab10.vallet
 
+import android.content.Context
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import org.junit.Assert
@@ -8,7 +9,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.core.methods.response.Web3ClientVersion
-
+import java.io.File
+import org.junit.rules.ExpectedException
+import org.junit.Rule
+import org.web3j.crypto.CipherException
 
 
 /**
@@ -17,19 +21,54 @@ import org.web3j.protocol.core.methods.response.Web3ClientVersion
 @RunWith(AndroidJUnit4::class)
 class ArtisUnitTest {
 
-    private var node : Web3j? = null
+    private lateinit var node : Web3j
+    private lateinit var appContext: Context
+
+    @Rule @JvmField
+    var exception = ExpectedException.none()
 
     @Before
     fun setArtisNode() {
-        val appContext = InstrumentationRegistry.getTargetContext()
+        appContext = InstrumentationRegistry.getTargetContext()
         node = Web3jManager.INSTANCE.getConnection(appContext)
+        Assert.assertNotNull(node)
+        Assert.assertNotNull(appContext)
+
     }
 
     @Test
     fun artis_testVersion() {
         Assert.assertNotNull(node)
-        val version = node!!.web3ClientVersion().send() as Web3ClientVersion
+        val version = node.web3ClientVersion().send() as Web3ClientVersion
         val clientVersion = version.getWeb3ClientVersion()
         Assert.assertEquals(clientVersion, "Parity//v1.8.3-beta-b49c44a-20171114/x86_64-linux-gnu/rustc1.21.0")
+    }
+
+    @Test
+    fun artis_createWallet() {
+        val password = "8e38DZDRT0Jy"
+        val walletFileName = Web3jManager.INSTANCE.createWallet(appContext,password)
+        Assert.assertNotNull(walletFileName)
+    }
+
+    @Test
+    fun artis_loadCredential() {
+        val password = "8e38DZDRT0Jy"
+        val walletFileName = Web3jManager.INSTANCE.createWallet(appContext, password)
+        val walletPath = File(appContext.filesDir, walletFileName)
+        val credential = Web3jManager.INSTANCE.loadCredential(password, walletPath.absolutePath)
+        Assert.assertNotNull(credential)
+    }
+
+    @Test
+    fun throwsInvalidPasswordProvided() {
+        exception.expect(CipherException::class.java)
+        exception.expectMessage("Invalid password provided")
+
+        val password = "8e38DZDRT0Jy"
+        val wrongPassword = "wrongpassword"
+        val walletFileName = Web3jManager.INSTANCE.createWallet(appContext, password)
+        val walletPath = File(appContext.filesDir, walletFileName)
+        Web3jManager.INSTANCE.loadCredential(wrongPassword, walletPath.absolutePath)
     }
 }
--- app/src/main/java/Web3jManager.kt
+++ app/src/main/java/Web3jManager.kt
@@ -1,9 +1,12 @@
 import android.content.Context
 import io.lab10.vallet.R
+import org.web3j.crypto.Credentials
 import org.web3j.protocol.core.methods.response.Web3ClientVersion
 import org.web3j.protocol.http.HttpService
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.Web3jFactory
+import org.web3j.crypto.WalletUtils
+import java.io.File
 
 
 /**
@@ -28,5 +31,19 @@ class Web3jManager private constructor(){
 
     fun getConnection(context: Context): Web3j{
        return Web3jFactory.build(HttpService(getNodeAddress(context)))
+    }
+
+    fun createWallet(context: Context, password: String): String {
+        val appDirectory = context.filesDir
+
+        val fileName = WalletUtils.generateNewWalletFile(
+                password, appDirectory, false)
+        return fileName
+    }
+
+    fun loadCredential(password: String, walletPath: String): Credentials {
+        return WalletUtils.loadCredentials(
+                password,
+                walletPath)
     }
 }
